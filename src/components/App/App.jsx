ec2f41414625598c0efe9d82e6e46019bdce8ff2
Marina Sereda <marina.gorb@gmail.com>
2022-11-21 18:46:50 +0200

    Add Statistics component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
 import user from 'data/user.json';
+import statData from 'data/statistic-data.json';
 import { Profile } from 'components/Profile';
+import { Statistics } from 'components/Statistics';
 import css from './App.module.css';
 
 export const App = () => {
@@ -12,6 +14,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statData} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,38 @@
+import { PropTypes } from 'prop-types';
+import { getRandomColor } from 'utils';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.stat__list}>
+        {stats.map(({ id, label, percentage }) => {
+          const color = getRandomColor();
+          return (
+            <li
+              className={css.item}
+              key={id}
+              style={{ backgroundColor: color }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
